b288eebaf32a6f5d3af0da299ca459590c66af26
Xghayor <cc.baqir@gmail.com>
2023-10-11 20:58:17 +0100

    Set up associations

--- student.rb
+++ student.rb
@@ -3,9 +3,14 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+    @classroom = []
+  end
+
+  def classroom=(classroom)
     @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
